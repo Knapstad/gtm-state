1c2ad1a42b04fd2031abefe8b4c7fa9cd67fb646
Bendik <knaben@OBOS.NO>
2020-03-04 12:22:37 +0100

    add if name

--- check_version.py
+++ check_version.py
@@ -1,29 +1,31 @@
 from google.oauth2 import service_account
+from google.cloud import storage
 import googleapiclient.discovery
 import json
+import requests
 
 
 with open("config.json") as file:
     config = json.load(file)
 
-GTM_ACCOUNT = config["gtmContainers"][0].split("_")[0]
-GTM_CONTAINER = config["gtmContainers"][0].split("_")[1]
+GTM_ACCOUNT = config["gtm_containers"][0].split("_")[0]
+GTM_CONTAINER = config["gtm_containers"][0].split("_")[1]
 SCOPES =["https://www.googleapis.com/auth/tagmanager.readonly","https://www.googleapis.com/auth/devstorage.read_write"] #https://www.googleapis.com/auth/tagmanager.edit.containers'
 SERVICE_ACCOUNT_FILE = "obos-225315-4adb95aa0d92.json"
 CREDENTIALS = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
+BLOB_NAME = config["gcs"]["blob_name"]
+BUCKET_NAME = config["gcs"]["bucket_name"]
 data = {}
 
 service = googleapiclient.discovery.build("tagmanager", "v2", credentials =CREDENTIALS )
 client = storage.Client(project="obos",credentials=CREDENTIALS)
 
-
 def get_gtm_version_data(service):
     data=service.accounts().containers().versions().live(parent=f"accounts/{GTM_ACCOUNT}/containers/{GTM_CONTAINER}").execute()
     return data
 
-def get_version(data):
+def get_version(data) -> str:
     if not data:
         data = get_gtm_version_data()
     return data["containerVersionId"]
@@ -35,7 +37,23 @@ def save_version_to_cloud(client, data: str, blob_name: str, bucket_name: str):
         blob = bucket.blob(blob_name)
     blob.upload_from_string(data)
 
+
 def load_version_from_cloud(client, blob_name: str, bucket_name: str):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.get_blob(blob_name)
     return blob.download_as_string()
+
+if __name__ == "__main__":
+    data = get_gtm_version_data(service)
+    version = get_version(data)
+    print(f"Live version: {version}")
+    cloud_version = load_version_from_cloud(client, BLOB_NAME, BUCKET_NAME)
+    cloud_version = str(cloud_version,"utf8")
+    print(f"Cloud version: {cloud_version}")
+
+    if cloud_version == version:
+        print("Is same version")
+    else:
+        message = f"New version: {version} \nName: {data['name']}\nDescription: {data['description'].capitalize()}\nLink: {data['tagManagerUrl']}"
+        print(message)
+        save_version_to_cloud(client,version,BLOB_NAME,BUCKET_NAME)
