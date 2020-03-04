64545ead3bed1d40ba7a19d83cdd3a6332c6c989
Bendik <knaben@OBOS.NO>
2020-03-04 14:06:15 +0100

    change name main

---
+++ check_version.py
@@ -0,0 +1,62 @@
+mainfrom google.oauth2 import service_account
+from google.cloud import storage
+import googleapiclient.discovery
+import json
+import requests
+
+
+with open("config.json") as file:
+    config = json.load(file)
+
+GTM_ACCOUNT = config["gtm_containers"][0].split("_")[0]
+GTM_CONTAINER = config["gtm_containers"][0].split("_")[1]
+SCOPES =["https://www.googleapis.com/auth/tagmanager.readonly","https://www.googleapis.com/auth/devstorage.read_write"] #https://www.googleapis.com/auth/tagmanager.edit.containers'
+SERVICE_ACCOUNT_FILE = "obos-225315-4adb95aa0d92.json"
+CREDENTIALS = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+BLOB_NAME = config["gcs"]["blob_name"]
+BUCKET_NAME = config["gcs"]["bucket_name"]
+HOOK = config["slack_hook"]
+data = {}
+
+service = googleapiclient.discovery.build("tagmanager", "v2", credentials =CREDENTIALS )
+client = storage.Client(project="obos",credentials=CREDENTIALS)
+
+def get_gtm_version_data(service):
+    data=service.accounts().containers().versions().live(parent=f"accounts/{GTM_ACCOUNT}/containers/{GTM_CONTAINER}").execute()
+    return data
+
+def get_version(data) -> str:
+    if not data:
+        data = get_gtm_version_data()
+    return data["containerVersionId"]
+
+def save_version_to_cloud(client, data: str, blob_name: str, bucket_name: str):
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.get_blob(blob_name)
+    if not blob:
+        blob = bucket.blob(blob_name)
+    blob.upload_from_string(data)
+
+
+def load_version_from_cloud(client, blob_name: str, bucket_name: str):
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.get_blob(blob_name)
+    return blob.download_as_string()
+
+def send_slack_message(hook, message):
+    payload={"text": message}
+    requests.post(hook, data=json.dumps(payload))
+
+if __name__ == "__main__":
+    data = get_gtm_version_data(service)
+    version = get_version(data)
+    cloud_version = load_version_from_cloud(client, BLOB_NAME, BUCKET_NAME)
+    cloud_version = str(cloud_version,"utf8")
+
+    if cloud_version == version:
+        pass
+    else:
+        message = f"New version: {version} \nName: {data['name']}\nDescription: {data['description'].capitalize()}\nLink: {data['tagManagerUrl']}"
+        send_slack_message(HOOK, message)
+        save_version_to_cloud(client,version,BLOB_NAME,BUCKET_NAME)
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import check_version
+import main
 
 def test_get_data():
     try:
-        data=check_version.get_gtm_version_data(check_version.service)
+        data=main.get_gtm_version_data(main.service)
         print("\nTesting get_data")
         assert data is not None
         print("Testing get_data     data is not None")
@@ -14,11 +14,11 @@ def test_get_data():
 
 def test_version():
     try:
-        data=check_version.get_gtm_version_data(check_version.service)
+        data=main.get_gtm_version_data(main.service)
         print("\nTesting get_version")
         assert data is not None
         print("Testing get_data     data is not None")
-        version = check_version.get_version(data)
+        version = main.get_version(data)
         assert len(version) > 0
         print("Testing get_version    version len > 0")
         assert type(version) == str
@@ -31,12 +31,12 @@ def test_version():
 def test_cloud_storage_save():
     print("\nTesting save_version_to_cloud")
     try:
-        check_version.save_version_to_cloud(check_version.client, "test data", "test.json", "gtm-state-storage")
+        main.save_version_to_cloud(main.client, "test data", "test.json", "gtm-state-storage")
     except Exception as e:
         print("Test storage save failed...")
         print(e)
     try:    
-        bucket = check_version.client.get_bucket("gtm-state-storage")
+        bucket = main.client.get_bucket("gtm-state-storage")
         test = bucket.get_blob("test.json")
         test_value = test.download_as_string()
         assert test_value == b"test data", "test is {test_value}" 
@@ -48,7 +48,7 @@ def test_cloud_storage_save():
 def test_cloud_storage_load():
     print("\nTesting load_version_from_cloud")
     try:
-        test = check_version.load_version_from_cloud(check_version.client, "test.json", "gtm-state-storage")
+        test = main.load_version_from_cloud(main.client, "test.json", "gtm-state-storage")
         assert test == b"test data", f"test is {test}"
         print("Load version passed")
     except Exception as e:
