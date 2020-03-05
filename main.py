e1d82fe4619fde0dc59e5a7dea6026099af8c834
Bendik <knaben@OBOS.NO>
2020-03-05 15:27:58 +0100

    add get_tag_changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from google.oauth2 import service_account
 from google.cloud import storage
+from collections import namedtuple
 import googleapiclient.discovery
 import json
 import requests
+
 
 
 with open("config.json") as file:
@@ -22,16 +24,16 @@ data = {}
 service = googleapiclient.discovery.build("tagmanager", "v2", credentials =CREDENTIALS )
 client = storage.Client(project="obos",credentials=CREDENTIALS)
 
-def get_gtm_version_data(service):
+def get_live_version_data(service):
     data=service.accounts().containers().versions().live(parent=f"accounts/{GTM_ACCOUNT}/containers/{GTM_CONTAINER}").execute()
     return data
 
-def get_version(data) -> str:
+def get_live_version(data) -> str:
     if not data:
-        data = get_gtm_version_data()
+        data = get_live_version_data()
     return data["containerVersionId"]
 
-def save_version_to_cloud(client, data: str, blob_name: str, bucket_name: str):
+def save_version_to_cloud(client: google.cloud.storage.client.Client, data: str, blob_name: str, bucket_name: str):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.get_blob(blob_name)
     if not blob:
@@ -39,19 +41,19 @@ def save_version_to_cloud(client, data: str, blob_name: str, bucket_name: str):
     blob.upload_from_string(data)
 
 
-def load_version_from_cloud(client, blob_name: str, bucket_name: str):
+def load_version_from_cloud(client: google.cloud.storage.client.Client, blob_name: str, bucket_name: str):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.get_blob(blob_name)
     return blob.download_as_string()
 
-def send_slack_message(hook, message):
+def send_slack_message(hook: str, message: str):
     payload={"text": message}
     requests.post(hook, data=json.dumps(payload))
 
 
 def check_version():
-    data = get_gtm_version_data(service)
-    version = get_version(data)
+    data = get_live_version_data(service)
+    version = get_live_version(data)
     cloud_version = load_version_from_cloud(client, BLOB_NAME, BUCKET_NAME)
     cloud_version = str(cloud_version,"utf8")
 
@@ -62,5 +64,23 @@ def check_version():
         send_slack_message(HOOK, message)
         save_version_to_cloud(client,version,BLOB_NAME,BUCKET_NAME)
 
+def get_version_data(version: str):
+    data=service.accounts().containers().versions().get(path=f"accounts/{GTM_ACCOUNT}/containers/{GTM_CONTAINER}/versions/{version}").execute()
+    return data
+
+def get_tag_changes(live: dict, oldversion: dict):
+    live_tags = [[i["tagId"], i["name"]] for i in live["tag"]]
+    old_tags = [i["tagId"] for i in oldversion["tag"]]
+    new = [i for i in live_tags if i not in old_tags]
+    removed = [i for i in old_tags if i not in live_tags]
+    changed = [i["tagId"] for i in live["tag"] for y in oldversion["tag"] if y["tagId"]==i["tagId"] and y["fingerprint"] != i["fingerprint"] ]
+    changes = namedtuple("changes", "new removed changed")
+
+    return (changes(new=new, removed=removed, changed=changed))
+
+
+
+
 if __name__ == "__main__":
+
     check_version()
